9a4a13e538e781ec856d6c3b46f774495c821bc4
Legroft <2471053840@qq.com>
2021-03-05 12:47:08 +0800

    添加注释.

--- index.py
+++ index.py
@@ -8,17 +8,20 @@ from openpyxl import load_workbook
 
 
 def check_token(p):  # 检验函数校验通过时返回None，否则返回错误消息
+    # token一般是36位的
     if len(p) != 36:
         return 'token格式错误！'
 
 
 def main():
+    # 设置网站标题
     set_env(title='自动打卡', output_animation=False)
+    # 说明
     put_markdown(r""" # 杭电自动健康打卡系统
     本系统尚不完善，存在许多bug,一切以实际效果为准.
     使用说明见[自动打卡使用说明](https://jinjis.cn/index.php/archives/hduzi-dong-jian-kang-da-ka-xi-tong.html)
     """,lstrip=True)
-
+    # 输入框等交互 返回字典
     data = input_group("信息", [
         input("请输入token", help_text='token获取方式见底部', type=TEXT, validate=check_token, name='token'),
         input("请输入省份", help_text='城市获取方式见底部 默认为浙江省杭州市江干区', type=NUMBER, value='7', name='p'),
@@ -39,7 +42,7 @@ def main():
 
 app = Flask(__name__)
 
-# task_func 为使用PyWebIO编写的任务函数
+# main 为使用PyWebIO编写的任务函数
 app.add_url_rule('/tool', 'webio_view', webio_view(main),
             methods=['GET', 'POST', 'OPTIONS'])  # 接口需要能接收GET、POST和OPTIONS请求
 
